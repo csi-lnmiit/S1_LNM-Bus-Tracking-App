e1f404b2a3cea51213253053572e21c1507329eb
itskshitizsh <itskshitizsh@gmail.com>
2018-03-19 14:06:21 +0530

    Added Splash Activity to Application.

---
+++ app/src/main/java/com/itskshitizsh/bustrajectory/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.itskshitizsh.bustrajectory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;  // 4 sec = 4000 milli seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
